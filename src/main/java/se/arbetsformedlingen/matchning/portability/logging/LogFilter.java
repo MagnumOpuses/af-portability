f67e1d7c66e693061354c1aeb13b708e4faac95a
Anton Wiklund <anton.wiklund@arbetsformedlingen.se>
2020-04-16 18:01:37 +0200

    Adjustments and additions: - Property "message", has been rearranged and have whitespaces introduced inbetween variables SCHEME, METHOD and URL. - Property "statusCode", is now an integer. - Property "meta.req.headers.host" is now composed of the servers name and port number. - Property "meta.req.url" and "meta.req.originalUrl" can now contain request query parameters. - Property "meta.req.query" is now composed of a JSONObject that includes all query parameters from the requested url. - Moved and renamed property "contentType" to "meta.req.headers.content-type". - Moved and renamed property "contentLength" to "meta.req.headers.content-length". - Renamed property "meta.req.headers.accept-encoding" to "meta.req.headers.accept". - Added property "meta.req.headers.origin". - Property "meta.responseTime" is now calculated (ms) and represented as an integer. - Flattened the JSON object creation. A bit easier to read with less nesting/indentation.

--- src/main/java/se/arbetsformedlingen/matchning/portability/logging/LogFilter.java
+++ src/main/java/se/arbetsformedlingen/matchning/portability/logging/LogFilter.java
@@ -46,89 +46,67 @@ public class LogFilter extends OncePerRequestFilter {
 
     protected void doLog(ContentCachingRequestWrapper requestWrapper, ContentCachingResponseWrapper responseWrapper, Long responseTime) {
 
-        String requestBody = "";
-        String responseBody = "";
-        boolean sessionTokenFound = false;
-        String sessionToken = " ";
+        // Create a dateformant compliant with ISO8601
+        final DateFormat ISO8601 = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm'Z'");
+        ISO8601.setTimeZone(TimeZone.getTimeZone("UTC"));
 
-        Configuration nullConfiguration = Configuration.builder().options(Option.DEFAULT_PATH_LEAF_TO_NULL).build();
+        // Parse query parameters into array of key-value pairs
+        final String queryString = requestWrapper.getQueryString();
+        final String url = requestWrapper.getRequestURI() + (queryString != null && queryString.length() > 0 ? "?" + queryString: "");
+        final List<NameValuePair> queryParams = URLEncodedUtils.parse(queryString, Charset.forName("UTF-8"));
 
-        String userAgent = requestWrapper.getHeader("user-agent");
-        String accept = requestWrapper.getHeader("accept");
-        String acceptEncoding = requestWrapper.getHeader("accept-encoding");
-        String connection = requestWrapper.getHeader("connection");
-        String origin = requestWrapper.getHeader("origin");
-        String referer = requestWrapper.getHeader("referer");
-        String secFetchMode = requestWrapper.getHeader("sec-fetch-mode");
-        String secFetchSite = requestWrapper.getHeader("sec-fetch-site");
-        String secFetchDest = requestWrapper.getHeader("sec-fetch-dest");
-        String protocol = requestWrapper.getProtocol();
-        String scheme = requestWrapper.getScheme().toUpperCase();
-        boolean isSecure = requestWrapper.isSecure();
-
-        //String host = requestWrapper.getRemoteAddr() + "/" + requestWrapper.getRemoteHost();
-        String host = String.format("%s:%s", requestWrapper.getServerName(), requestWrapper.getServerPort());
-        String method = requestWrapper.getMethod();
-        int statusCode = responseWrapper.getStatusCode();
-        String contentType = responseWrapper.getContentType();
-        String contentLength = String.valueOf(responseWrapper.getContentAsByteArray().length);
-
-        String queryString = requestWrapper.getQueryString();
-        String url = requestWrapper.getRequestURI() + (queryString.length() > 0 ? "?" + queryString: "");
-        List<NameValuePair> queryParams = URLEncodedUtils.parse(queryString, Charset.forName("UTF-8"));
-
-        /*if (requestWrapper.getParameterValues("sessionToken") != null) {
-            sessionToken = requestWrapper.getParameterValues("sessionToken")[0];
-            sessionTokenFound = true;
-        }*/
-
-        TimeZone tz = TimeZone.getTimeZone("UTC");
-        DateFormat df = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm'Z'"); // Quoted "Z" to indicate UTC, no timezone offset
-        df.setTimeZone(tz);
-        String time = df.format(new Date());
-
-        final String jsonString;
+        // Produce JSON structure and write to STDOUT
         try {
-            JSONObject jsonQueryParams = new JSONObject();
+            final JSONObject reqHeaders = new JSONObject()
+                    .put("host", String.format("%s:%s", requestWrapper.getServerName(), requestWrapper.getServerPort()))
+                    .put("connection", requestWrapper.getHeader("connection"))
+                    .put("user-agent", requestWrapper.getHeader("user-agent"))
+                    .put("accept", requestWrapper.getHeader("accept"))
+                    .put("accept-encoding", requestWrapper.getHeader("accept-encoding"))
+                    .put("origin", requestWrapper.getHeader("origin"))
+                    .put("referer", requestWrapper.getHeader("referer"))
+                    .put("sec-fetch-dest", requestWrapper.getHeader("sec-fetch-dest"))
+                    .put("sec-fetch-mode", requestWrapper.getHeader("sec-fetch-mode"))
+                    .put("sec-fetch-site", requestWrapper.getHeader("sec-fetch-site"))
+                    .put("method", requestWrapper.getMethod())
+                    .put("httpVersion", requestWrapper.getProtocol())
+                    .put("content-type", responseWrapper.getContentType())
+                    .put("content-length", String.valueOf(responseWrapper.getContentAsByteArray().length));
+
+            final JSONObject reqQuery = new JSONObject();
             for (NameValuePair param : queryParams) {
-                jsonQueryParams.put(param.getName(), param.getValue());
+                reqQuery.put(param.getName(), param.getValue());
             }
 
-            jsonString = new JSONObject()
-                    .put("meta", new JSONObject()
-                            .put("req", new JSONObject()
-                                    .put("url", url)
-                                    .put("headers", new JSONObject()
-                                            .put("host", host)
-                                            .put("connection", connection)
-                                            .put("user-agent", userAgent)
-                                            .put("accept", accept)
-                                            .put("accept-encoding", acceptEncoding)
-                                            .put("origin", origin)
-                                            .put("content-type", contentType)
-                                            .put("content-length", contentLength)
-                                            .put("referer", referer)
-                                            .put("sec-fetch-dest", secFetchDest)
-                                            .put("sec-fetch-mode", secFetchMode)
-                                            .put("sec-fetch-site", secFetchSite))
-                                    .put("method", method)
-                                    .put("httpVersion", protocol)
-                                    .put("originalUrl", url)
-                                    .put("query", jsonQueryParams))
-                            .put("res", new JSONObject()
-                                    .put("statusCode", statusCode))
-                            .put("responseTime", Math.toIntExact(responseTime)))
-                    .put("message", String.format("%s %s %s", scheme, method, url))
-                    .put("timestamp", time)
-                    .put("level", "info")
-                    .toString();
+            final JSONObject req = new JSONObject()
+                    .put("headers", reqHeaders)
+                    .put("query", reqQuery)
+                    .put("originalUrl", url)
+                    .put("url", url)
+                    .put("method", requestWrapper.getMethod())
+                    .put("httpVersion", requestWrapper.getProtocol());
 
-            System.out.println(jsonString);
-            System.out.println("OK");
+            final JSONObject res = new JSONObject()
+                    .put("statusCode", responseWrapper.getStatusCode());
+
+            final JSONObject meta = new JSONObject()
+                    .put("req", req)
+                    .put("res", res)
+                    .put("responseTime", responseTime);
+
+            final JSONObject entry = new JSONObject()
+                    .put("meta", meta)
+                    .put("level", "info")
+                    .put("message", String.format("%s %s %s",
+                            requestWrapper.getScheme().toUpperCase(),
+                            requestWrapper.getMethod(),
+                            url))
+                    .put("timestamp", ISO8601.format(new Date()));
+
+            System.out.println(entry.toString());
         } catch (JSONException e) {
             e.printStackTrace();
         }
-
     }
 
     @Override
