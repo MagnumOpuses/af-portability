e259cef5a79cc003d864489841bb6aa1448e2156
taz-jt <50823068+taz-jt@users.noreply.github.com>
2020-05-18 12:22:15 +0200

    Stage merge to Master for pre-release 1.2.0-beta (#38)
    * fixing logging
    
    * fixing teh logging
    
    * Corrections and additions
    
    * Adjustments and additions:
    - Property "message", has been rearranged and have whitespaces introduced inbetween variables SCHEME, METHOD and URL.
    - Property "statusCode", is now an integer.
    - Property "meta.req.headers.host" is now composed of the servers name and port number.
    - Property "meta.req.url" and "meta.req.originalUrl" can now contain request query parameters.
    - Property "meta.req.query" is now composed of a JSONObject that includes all query parameters from the requested url.
    - Moved and renamed property "contentType" to "meta.req.headers.content-type".
    - Moved and renamed property "contentLength" to "meta.req.headers.content-length".
    - Renamed property "meta.req.headers.accept-encoding" to "meta.req.headers.accept".
    - Added property "meta.req.headers.origin".
    - Property "meta.responseTime" is now calculated (ms) and represented as an integer.
    - Flattened the JSON object creation. A bit easier to read with less nesting/indentation.
    
    * cleaning and modifying jenkinsfile (#36)
    
    * Jenkins/modify (#37)
    
    * cleaning and modifying jenkinsfile
    
    * modifying jenkins pipeline stages 1
    
    Co-authored-by: Markella Nikolopoulou-Themeli <markella.nikolopoulou-themeli@arbetsformedlingen.se>
    Co-authored-by: markella-n-th <51362475+markella-n-th@users.noreply.github.com>
    Co-authored-by: Anton Wiklund <anton.wiklund@arbetsformedlingen.se>

--- src/main/java/se/arbetsformedlingen/matchning/portability/logging/LogFilter.java
+++ src/main/java/se/arbetsformedlingen/matchning/portability/logging/LogFilter.java
@@ -3,6 +3,9 @@ package se.arbetsformedlingen.matchning.portability.logging;
 import com.jayway.jsonpath.Configuration;
 import com.jayway.jsonpath.JsonPath;
 import com.jayway.jsonpath.Option;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.utils.URLEncodedUtils;
+import org.json.JSONException;
 import org.json.JSONObject;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
@@ -14,11 +17,16 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.net.URI;
+import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
-import java.util.HashMap;
-import java.util.Map;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.*;
+
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.LogManager;
+
 import java.util.regex.Pattern;
 
 /**
@@ -36,36 +44,69 @@ public class LogFilter extends OncePerRequestFilter {
 
     private static Logger logger = LogManager.getLogger(LogFilter.class.getCanonicalName());
 
-    protected void doLog(ContentCachingRequestWrapper requestWrapper, ContentCachingResponseWrapper responseWrapper) {
-        String requestBody = "";
-        String responseBody = "";
-        Map<String, String> logMap = new HashMap<>();
-        boolean sessionTokenFound = false;
+    protected void doLog(ContentCachingRequestWrapper requestWrapper, ContentCachingResponseWrapper responseWrapper, Long responseTime) {
 
-        Configuration nullConfiguration = Configuration.builder().options(Option.DEFAULT_PATH_LEAF_TO_NULL).build();
+        // Create a dateformant compliant with ISO8601
+        final DateFormat ISO8601 = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm'Z'");
+        ISO8601.setTimeZone(TimeZone.getTimeZone("UTC"));
 
-        logMap.put("Host", requestWrapper.getRemoteAddr() + " ---- " + requestWrapper.getRemoteHost());
-        logMap.put("Method", requestWrapper.getMethod());
-        logMap.put("URI", requestWrapper.getRequestURI());
+        // Parse query parameters into array of key-value pairs
+        final String queryString = requestWrapper.getQueryString();
+        final String url = requestWrapper.getRequestURI() + (queryString != null && queryString.length() > 0 ? "?" + queryString: "");
+        final List<NameValuePair> queryParams = URLEncodedUtils.parse(queryString, Charset.forName("UTF-8"));
 
-        if (requestWrapper.getParameterValues("sessionToken") != null) {
-            logMap.put("SessionToken", requestWrapper.getParameterValues("sessionToken")[0]);
-            sessionTokenFound = true;
+        // Produce JSON structure and write to STDOUT
+        try {
+            final JSONObject reqHeaders = new JSONObject()
+                    .put("host", String.format("%s:%s", requestWrapper.getServerName(), requestWrapper.getServerPort()))
+                    .put("connection", requestWrapper.getHeader("connection"))
+                    .put("user-agent", requestWrapper.getHeader("user-agent"))
+                    .put("accept", requestWrapper.getHeader("accept"))
+                    .put("accept-encoding", requestWrapper.getHeader("accept-encoding"))
+                    .put("origin", requestWrapper.getHeader("origin"))
+                    .put("referer", requestWrapper.getHeader("referer"))
+                    .put("sec-fetch-dest", requestWrapper.getHeader("sec-fetch-dest"))
+                    .put("sec-fetch-mode", requestWrapper.getHeader("sec-fetch-mode"))
+                    .put("sec-fetch-site", requestWrapper.getHeader("sec-fetch-site"))
+                    .put("method", requestWrapper.getMethod())
+                    .put("httpVersion", requestWrapper.getProtocol())
+                    .put("content-type", responseWrapper.getContentType())
+                    .put("content-length", String.valueOf(responseWrapper.getContentAsByteArray().length));
+
+            final JSONObject reqQuery = new JSONObject();
+            for (NameValuePair param : queryParams) {
+                reqQuery.put(param.getName(), param.getValue());
+            }
+
+            final JSONObject req = new JSONObject()
+                    .put("headers", reqHeaders)
+                    .put("query", reqQuery)
+                    .put("originalUrl", url)
+                    .put("url", url)
+                    .put("method", requestWrapper.getMethod())
+                    .put("httpVersion", requestWrapper.getProtocol());
+
+            final JSONObject res = new JSONObject()
+                    .put("statusCode", responseWrapper.getStatusCode());
+
+            final JSONObject meta = new JSONObject()
+                    .put("req", req)
+                    .put("res", res)
+                    .put("responseTime", responseTime);
+
+            final JSONObject entry = new JSONObject()
+                    .put("meta", meta)
+                    .put("level", "info")
+                    .put("message", String.format("%s %s %s",
+                            requestWrapper.getScheme().toUpperCase(),
+                            requestWrapper.getMethod(),
+                            url))
+                    .put("timestamp", ISO8601.format(new Date()));
+
+            System.out.println(entry.toString());
+        } catch (JSONException e) {
+            e.printStackTrace();
         }
-
-        Long duration = (Long)requestWrapper.getRequest().getAttribute("duration");
-
-        logMap.put("Status code", String.valueOf(responseWrapper.getStatusCode()));
-
-        if (duration != null) {
-            logMap.put("duration", String.valueOf(duration));
-        }
-        logMap.put("Content Type", responseWrapper.getContentType());
-
-        logMap.put("Content Length", String.valueOf(responseWrapper.getContentAsByteArray().length) + " bytes");
-
-        JSONObject message = new JSONObject(logMap);
-        logger.info(message);
     }
 
     @Override
@@ -75,8 +116,10 @@ public class LogFilter extends OncePerRequestFilter {
         ContentCachingRequestWrapper requestWrapper = new ContentCachingRequestWrapper(request);
         ContentCachingResponseWrapper responseWrapper = new ContentCachingResponseWrapper(response);
 
+        long start = System.currentTimeMillis();
         filterChain.doFilter(requestWrapper, responseWrapper);
-        doLog(requestWrapper, responseWrapper);
+        long responseTime = System.currentTimeMillis() - start;
+        doLog(requestWrapper, responseWrapper, responseTime);
         responseWrapper.copyBodyToResponse();
     }
 
