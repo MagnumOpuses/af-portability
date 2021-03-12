16cab48ec63a168b69912cdcb7b83d45ac03f7cd
Anton Wiklund <anton.wiklund@arbetsformedlingen.se>
2021-03-12 09:17:56 +0100

    Fetch extra 'company_name' detail from the apikey manager data source… (#43)
    * Fetch extra 'company_name' detail from the apikey manager data source upon apikey verification.
    
    * Added some getters to the ApiKeys entity.

--- src/main/java/se/arbetsformedlingen/matchning/portability/model/ApiKeys.java
+++ src/main/java/se/arbetsformedlingen/matchning/portability/model/ApiKeys.java
@@ -13,18 +13,41 @@ public class ApiKeys {
     private String apikey;
     private Long api_id;
     private String email;
+    private String company_name;
 
-    public ApiKeys() {}
+    public ApiKeys() {
+    }
 
-    public ApiKeys(Long id, String apikey, Long api_id, String email) {
+    public ApiKeys(final Long id, final String apikey, final Long api_id, final String email, final String company_name) {
         this.id = id;
         this.apikey = apikey;
         this.api_id = api_id;
         this.email = email;
+        this.company_name = company_name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getApikey() {
+        return apikey;
+    }
+
+    public Long getApiId() {
+        return api_id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getCompanyName() {
+        return company_name;
     }
 
     @Override
     public String toString() {
-        return this.id + " -- " + this.apikey + " -- " + this.email;
+        return this.id + " -- " + this.apikey + " -- " + this.email + " -- " + this.company_name;
     }
 }
